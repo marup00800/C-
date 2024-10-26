adb0c71cd871be1add7b7ccda8d1356da5ea389c
Marup Hossain <76910708+marup00800@users.noreply.github.com>
2024-10-26 20:46:01 +0600

    Add files via upload

---
+++ Switch.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    int a;
+
+    cout<<"Enter Month Number 1-12 : ";
+    cin>> a;
+
+    switch(a){
+
+        case 1:
+        cout<<"January";
+        break;
+
+        case 2:
+        cout<<"February";
+        break;
+
+        case 3:
+        cout<<"March";
+        break;
+
+        case 4:
+        cout<<"April";
+        break;
+
+        case 5:
+        cout<<"May";
+        break;
+
+        case 6:
+        cout<<"June";
+        break;
+
+        case 7:
+        cout<<"July";
+        break;
+
+        case 8:
+        cout<<"August";
+        break;
+
+        case 9:
+        cout<<"September";
+        break;
+
+        case 10:
+        cout<<"October";
+        break;
+
+        case 11:
+        cout<<"November";
+        break;
+
+        case 12:
+        cout<<"December";
+        break;
+
+
+    }
+
+}
